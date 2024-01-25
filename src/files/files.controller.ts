a4bf1a7765fe1ee3383057f1731d7a680a46d52e
dixonAnatoAscencio <dixonjesusanato@gmail.com>
2024-01-25 09:14:56 -0500

    add files modules

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -9,15 +9,32 @@ import {
   UploadedFile,
   UseInterceptors,
   BadRequestException,
+  Res,
 } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express'; //carga de archivos especifica para express no sirve para fastify
 import { fileFilter, fileNamer } from './helpers';
 import { diskStorage } from 'multer';
+import { Response } from 'express';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('files')
 export class FilesController {
-  constructor(private readonly filesService: FilesService) {}
+  constructor(
+    private readonly filesService: FilesService,
+    private readonly configService: ConfigService,
+    ) {}
+
+  @Get('product/:imageName')
+  findProductImage(
+    @Res() res: Response,
+    @Param('imageName') imageName: string,
+  ){
+
+    const path = this.filesService.getStaticProductImage(imageName);
+
+    res.sendFile(path);
+  }
 
   @Post('product')
   @UseInterceptors(FileInterceptor('file', {
@@ -37,10 +54,12 @@ export class FilesController {
     if(!file) {
       throw new BadRequestException('Make sure that the file is an image');
     }
+
+    const secureUrl = `${ this.configService.get('HOST_API') }files/product/${ file.filename }`;
    
     return {
-      fileName: file.originalname
-    };
+      secureUrl
+    }
   }
 
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const logger = new Logger('Bootstrap');
 
 
   app.setGlobalPrefix('api');
@@ -12,9 +15,15 @@ async function bootstrap() {
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
-    })
-  )
+    }),
+  ),
 
-  await app.listen(3000);
+  ServeStaticModule.forRoot({
+    rootPath: join(__dirname, '..', 'public'),
+    renderPath: '/public',
+  })
+
+    await app.listen(process.env.PORT || 3000);
+    logger.log(`App running on port ${ process.env.PORT || 3000 }`);
 }
 bootstrap();
